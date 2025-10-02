1721e3d6d3cba11843eec8c813cf3627b00c2243
isnuraina <nurane.ismayilzade19@gmail.com>
2025-10-02 10:49:26 +0400

    Mapping is added

---
+++ Multishop.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Multishop.Catalog.DTOs.CategoryDtos;
+using Multishop.Catalog.DTOs.ProductDetailDtos;
+using Multishop.Catalog.DTOs.ProductImageDtos;
+using Multishop.Catalog.Entities;
+
+namespace Multishop.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIdCategoryDto>().ReverseMap();
+
+
+            CreateMap<Product, ResultProductDetailDto>().ReverseMap();
+            CreateMap<Product,  CreateCategoryDto>().ReverseMap();
+            CreateMap<Product, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Product, GetByIdCategoryDto>().ReverseMap();
+
+
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, GetByIdProductDetailDto>().ReverseMap();
+
+
+            CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, UpdateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, GetByIdProductImageDto>().ReverseMap();
+
+
+
+
+
+        }
+    }
+}
