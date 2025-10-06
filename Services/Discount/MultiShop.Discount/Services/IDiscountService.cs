2f66ca7af99a183869d845bd52934c9409d1baa8
isnuraina <nurane.ismayilzade19@gmail.com>
2025-10-06 15:11:12 +0400

    Discount microservice is added to project

---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Discount.DTOs;
+
+namespace MultiShop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync();
+        Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto);
+        Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id); 
+
+    }
+}
