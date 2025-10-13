07a23cf5f37a1c5ef748234a2fe4cbe4fd3e81c1
isnuraina <nurane.ismayilzade19@gmail.com>
2025-10-13 17:59:37 +0400

    Docker is coming

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,7 +1,14 @@
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
 
 
 builder.Services.AddScoped<GetAddressQueryHandler>();
